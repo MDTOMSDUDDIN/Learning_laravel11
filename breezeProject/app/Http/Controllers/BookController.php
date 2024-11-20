ee0ea59cae5df698ad9cf5364dcdae9c2ed7375c
MD TOMAS UDDIN <143870012+MDTOMSDUDDIN@users.noreply.github.com>
2024-11-20 20:12:46 +0600

    laravel learning repository started

---
+++ breezeProject/app/Http/Controllers/BookController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    function showbook(Request $request){
+        echo "Hello from our laravel application";
+    }
+}
